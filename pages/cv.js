9a2335e84faad7d3879529812d3bd81db3e0765c
Timmy <62808178+timothymaxwellsummers@users.noreply.github.com>
2022-05-19 17:47:24 +0200

    solved build issue

--- pages/cv.js
+++ pages/cv.js
@@ -13,7 +13,7 @@ const DynamicComponentWithNoSSR = dynamic(
 export default function Home() {
   return (
     <object data="/Summers_CV_Eng.pdf" type="application/pdf" className={styles.pdf}>
-      <p>Sorry something didn't workout :/ <a href="/Summers_CV_Eng.pdf">find the PDF here!</a></p>
+      <p>Sorry something didn&apos;t workout :/ <a href="/Summers_CV_Eng.pdf">find the PDF here!</a></p>
   </object>
   );
 }
